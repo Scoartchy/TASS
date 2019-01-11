52a2b5688a2a6af0f162d6e777b57b4eeccd66b9
Scoartchy <scoartchy@gmail.com>
2019-01-11 23:41:06 +0100

    added moddyfied scholar.py script

---
+++ tass.bat
@@ -0,0 +1,15 @@
+::scholar.py -c 1 --author "albert einstein" --phrase "quantum theory"
+
+::scholar.py -c 1 --author "albert einstein" --phrase "quantum theory" --citation bt
+
+::scholar.py --txt-globals --author "albert einstein" | grep '\[G\]' | grep Results
+
+::scholar.py -c 1 --author "Stephen Hawking" --phrase "quantum theory" 
+
+::scholar.py -c 1 -C 17749203648027613321 --citation bt
+
+::scholar.py --citations-only -c 1 --author "albert einstein" --phrase "quantum theory"
+
+scholar.py --citations-only -c 1 --author "albert einstein" --phrase "quantum theory"
+
+pause
